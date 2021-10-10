e2a57e9be3f42475a7afaaea932d91d6efe03121
RahimliZakir <zakirer@code.edu.az>
2021-10-10 10:25:53 +0400

    Implementation Project

---
+++ Riode.Template.Solution/Riode.Template.WebUI/Models/DataContext/RiodeDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Riode.Template.WebUI.Models.DataContext
+{
+    public class RiodeDbContext : DbContext
+    {
+        public RiodeDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+    }
+}
