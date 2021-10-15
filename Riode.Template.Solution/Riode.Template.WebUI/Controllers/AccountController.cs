411cb91943894c7eeba1aaa14ef5789064501a20
RahimliZakir <zakirer@code.edu.az>
2021-10-16 03:45:57 +0400

    Several Style Fixes

---
+++ Riode.Template.Solution/Riode.Template.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Riode.Template.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+        public IActionResult WishList()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Riode.Template.Solution/Riode.Template.WebUI/Models/Entity/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Riode.Template.WebUI.Models.Entity
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+
+    }
+}
