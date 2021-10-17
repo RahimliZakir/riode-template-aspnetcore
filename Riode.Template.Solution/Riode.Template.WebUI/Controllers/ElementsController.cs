a6a41f21d5f4879901367f25187192875c568f7e
RahimliZakir <zakirer@code.edu.az>
2021-10-17 05:52:03 +0400

    The Views Of Elements Controller Created

---
+++ Riode.Template.Solution/Riode.Template.WebUI/Controllers/ElementsController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Riode.Template.WebUI.Controllers
+{
+    public class ElementsController : Controller
+    {
+        public IActionResult Products()
+        {
+            return View();
+        }
+
+        public IActionResult Typography()
+        {
+            return View();
+        }
+
+        public IActionResult Titles()
+        {
+            return View();
+        }
+
+        public IActionResult ProductCategory()
+        {
+            return View();
+        }
+
+        public IActionResult Buttons()
+        {
+            return View();
+        }
+
+        public IActionResult Accordions()
+        {
+            return View();
+        }
+
+        public IActionResult AlertAndNotification()
+        {
+            return View();
+        }
+
+        public IActionResult Tabs()
+        {
+            return View();
+        }
+
+        public IActionResult Testimonials()
+        {
+            return View();
+        }
+
+        public IActionResult BlogPosts()
+        {
+            return View();
+        }
+
+        public IActionResult Instagrams()
+        {
+            return View();
+        }
+
+        public IActionResult CallToAction()
+        {
+            return View();
+        }
+
+        public IActionResult IconBoxes()
+        {
+            return View();
+        }
+
+        public IActionResult Icons()
+        {
+            return View();
+        }
+    }
+}
