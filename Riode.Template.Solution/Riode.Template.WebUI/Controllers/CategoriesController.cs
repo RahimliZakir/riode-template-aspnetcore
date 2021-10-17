89bd58576a24c10a17007e1f3dcaf656645e9cde
RahimliZakir <zakirer@code.edu.az>
2021-10-17 04:01:45 +0400

    The Views Of Categories Controller Created

---
+++ Riode.Template.Solution/Riode.Template.WebUI/Controllers/CategoriesController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Riode.Template.WebUI.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // Variations - 1
+        public IActionResult BannerWithSidebar()
+        {
+            return View();
+        }
+
+        public IActionResult BoxedBanner()
+        {
+            return View();
+        }
+
+        public IActionResult InfiniteAJAXScroll()
+        {
+            return View();
+        }
+
+        public IActionResult HorizontalFilter()
+        {
+            return View();
+        }
+
+        public IActionResult NavigationFilter()
+        {
+            return View();
+        }
+
+        public IActionResult OffCanvasFilter()
+        {
+            return View();
+        }
+
+        public IActionResult RightToggleSidebar()
+        {
+            return View();
+        }
+        // Variations - 1
+
+        // Variations - 2
+        public IActionResult ThreeColumnsMode()
+        {
+            return View();
+        }
+
+        public IActionResult FourColumnsMode()
+        {
+            return View();
+        }
+
+        public IActionResult FiveColumnsMode()
+        {
+            return View();
+        }
+
+        public IActionResult SixColumnsMode()
+        {
+            return View();
+        }
+
+        public IActionResult SevenColumnsMode()
+        {
+            return View();
+        }
+
+        public IActionResult EightColumnsMode()
+        {
+            return View();
+        }
+
+        public IActionResult ListMode()
+        {
+            return View();
+        } 
+        // Variations - 1
+    }
+}
